b9b25c7177eda455e70303d1da63793d3a325aeb
Kabir Tamari <rabbit.bonex@gmail.com>
2019-07-25 12:44:13 +0545

    redesign profolio

---
+++ main/models.py
@@ -0,0 +1,35 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# import uuid 
+
+# # Create your models here.
+# class LangaugeUsed(models.Model):
+#     name = models.CharField()
+    
+#     def __str__(self):
+#         return self.name
+
+# class CustomUser(AbstractUser):
+#     is_admin = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.first_name + self.last_name
+
+# class ProjectType(models.Model):
+#     name = models.CharField()
+
+#     def __str__(self):
+#         return self.name
+
+
+
+# class project_details(models.Model):
+#     name = models.CharField(max_length = 200)
+#     detail = models.TextField()
+#     image = models.URLField()
+
+
+#     def __str__(self):
+#         return self.name
+
+
